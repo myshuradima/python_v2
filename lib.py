326d42067323f40d9dafcd2a414c038abb2bb0b2
dima <trofolgar.d@gmail.com>
2018-11-27 14:24:36 +0200

    exam

--- lib.py
+++ lib.py
@@ -1,10 +1,14 @@
 import re
 sdudent_name=input("student name")
-if (re.match(r"^[A-Z]{2}\-\d{8}$",student_name)):
-    return student_name
-else:
-company = input("company")
-if (re.match(r"^[a-z]{0,10}\d*$",student_name)):
-    return company
-
-    return False
+def student_valid():
+    sdudent_name = input("student name")
+    if (re.match(r"^[A-Z]{2}\-\d{8}$",student_name)):
+        return student_name
+    else:
+        return False
+def company_valid():
+    company = input("company")
+    if (re.match(r"^[a-z]{0,10}\d*$",student_name)):
+        return company
+    else:
+        return False
